9e2701a0b0cda6a44af048a253822ceb8f91b5a1
Heinzerbergh <123276387+Heinzerbergh@users.noreply.github.com>
2023-12-06 17:03:24 +0000

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,23 @@
-
-// Add an event listener to an element with the id 'share-button'
 document.getElementById('share-button').addEventListener('click', function() {
     // Get the current URL of the page
     var urlToCopy = window.location.href;
 
     // Create a new input element dynamically
     var inputElement = document.createElement('input');
-
-    // Set the value of the input element to the current URL
     inputElement.value = urlToCopy;
-
-    // Append the input element to the body of the document
     document.body.appendChild(inputElement);
 
     // Select the content inside the input element
     inputElement.select();
 
-    // Execute the 'copy' command to copy the selected content to the clipboard
-    document.execCommand('copy');
-
-    // Remove the temporary input element from the body
-    document.body.removeChild(inputElement);
-
-    // Display an alert to notify the user that the URL has been copied
-    alert('URL copied to clipboard: ' + urlToCopy);
+    try {
+        // Use the Clipboard API to copy the URL to the clipboard
+        document.execCommand('copy');
+        alert('URL copied to clipboard: ' + urlToCopy);
+    } catch (err) {
+        console.error('Unable to copy URL to clipboard', err);
+    } finally {
+        // Remove the temporary input element from the body
+        document.body.removeChild(inputElement);
+    }
 });
